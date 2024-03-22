7f9a7b19b668741c71cf0e4fb475a7b7ba42793c
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2024-03-22 14:27:59 +1100

    updated map, when user zoom in

--- public/javascripts/getMap.js
+++ public/javascripts/getMap.js
@@ -6,14 +6,47 @@ function GetMap() {
     mapTypeId: Microsoft.Maps.MapTypeId.aerial,
     zoom: 12,
   });
-  const center = map.getCenter();
-  const location = new Microsoft.Maps.Location(-34.3689, 150.8932);
-  // create a pin on the map
-  const pin = new Microsoft.Maps.Pushpin(location, {
-    title: 'Corrimal',
-    subTitle: 'Des of furniture',
-    text: '1',
-  });
-  // add to the map
-  map.entities.push(pin);
+
+  // Function to add pins to the map with initial content
+  function addPinsWithContent() {
+    // Provide location to add pin on map
+    const location = new Microsoft.Maps.Location(-34.3689, 150.8932);
+    // Create a pin on the map
+    const pin = new Microsoft.Maps.Pushpin(location, {
+      title: 'Corrimal',
+      subTitle: 'Description of furniture',
+      text: '1',
+    });
+    // Add the pin to the map
+    map.entities.push(pin);
+  }
+
+  // Function to update pin content based on zoom level
+  function updatePinContentBasedOnZoomLevel(zoomLevel) {
+    // Example: Update pin text based on zoom level
+    const pin = map.entities.get(0); // Assuming only one pin is added to the map
+    if (zoomLevel > 13) {
+      pin.setOptions({
+        text: 'More detailed content', // Update pin text with more detailed information
+      });
+    } else {
+      pin.setOptions({
+        text: '1', // Reset pin text to default if zoom level is less than or equal to 15
+      });
+    }
+  }
+
+  // Function to handle map zoom level change
+  function handleZoomLevelChange() {
+    const currentZoomLevel = map.getZoom(); // Get current zoom level of the map
+    console.log(currentZoomLevel);
+    // Update pin content based on current zoom level
+    updatePinContentBasedOnZoomLevel(currentZoomLevel);
+  }
+
+  // Event listener for map zoom level change
+  Microsoft.Maps.Events.addHandler(map, 'viewchange', handleZoomLevelChange);
+
+  // Add pins to the map with initial content
+  addPinsWithContent();
 }
