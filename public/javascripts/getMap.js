a93fd8991b5b85366ca3d5a549ffb655f5a38c8b
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2024-03-22 14:13:11 +1100

    added pin on map view

--- public/javascripts/getMap.js
+++ public/javascripts/getMap.js
@@ -1,8 +1,19 @@
 function GetMap() {
-  var map = new Microsoft.Maps.Map('#myMap', {
+  const map = new Microsoft.Maps.Map('#myMap', {});
+  map.setView({
     credentials: 'Your Bing Maps Key',
     center: new Microsoft.Maps.Location(-34.3915, 150.85),
     mapTypeId: Microsoft.Maps.MapTypeId.aerial,
     zoom: 12,
   });
+  const center = map.getCenter();
+  const location = new Microsoft.Maps.Location(-34.3689, 150.8932);
+  // create a pin on the map
+  const pin = new Microsoft.Maps.Pushpin(location, {
+    title: 'Corrimal',
+    subTitle: 'Des of furniture',
+    text: '1',
+  });
+  // add to the map
+  map.entities.push(pin);
 }
